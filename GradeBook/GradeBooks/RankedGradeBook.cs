f46c497ec28063054935420dcffb389b9c8abf18
Jack Averill <jfaverill@gmail.com>
2020-08-10 12:25:52 -0400

    Update RankedGradeBook.cs

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using GradeBook.Enums;
 
 namespace GradeBook.GradeBooks
@@ -8,6 +9,52 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name) : base(name)
         {
             this.Type = GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if(Students.Count < 5)
+            {
+                throw new InvalidOperationException();
+            }
+
+            var limitForA = (int) (Students.Count * 0.2);
+            var limitForB = (int) (Students.Count * 0.4);
+            var limitForC = (int) (Students.Count * 0.6);
+            var limitForD = (int) (Students.Count * 0.8);
+            var averageGrades = new List<double>();
+
+            foreach (Student student in Students)
+            {
+                averageGrades.Add(student.AverageGrade);
+            }
+
+            averageGrades.Sort();
+
+            for (int i = 0; i < Students.Count; i++)
+            {
+                if (averageGrade > averageGrades[i])
+                {
+                    if (i + 1 <= limitForA)
+                    {
+                        return 'A';
+                    }
+                    else if (i + 1 <= limitForB)
+                    {
+                        return 'B';
+                    }
+                    else if (i + 1 <= limitForC)
+                    {
+                        return 'C';
+                    }
+                    else if (i + 1 <= limitForD)
+                    {
+                        return 'D';
+                    }
+                }
+            }
+
+            return 'F';
         }
     }
 }
